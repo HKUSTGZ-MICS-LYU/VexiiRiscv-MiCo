sycamoremoon/VexiiRiscv

Add Guest translation indicator for MMU
Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

src/main/scala/vexiiriscv/memory/Service.scala
@@ -102,6 +102,7 @@ trait AddressTranslationService extends Area {
 
 case class AddressTranslationReq(
   PRE_ADDRESS: Payload[UInt],
+  FROM_GUEST: Payload[Bool],
   FORCE_PHYSICAL: Payload[Bool]
 )
 

HKUSTGZ-MICS-LYU/VexiiRiscv-MiCo

src/main/scala/vexiiriscv/memory/Service.scala
@@ -105,6 +105,7 @@ case class AddressTranslationReq(
   LOAD: Payload[Bool],
   STORE: Payload[Bool],
   EXECUTE: Payload[Bool],
+  FORCE_GUEST: Payload[Bool],
   FORCE_PHYSICAL: Payload[Bool]
 )
 